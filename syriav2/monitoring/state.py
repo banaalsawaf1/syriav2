072a70d6130b0e8c7317f550295dd6dd3a8abc9c
banaalsawaf1 <banaalsawaf1@gmail.com>
2026-01-11 18:41:28 +0300

    Update state.py

--- syriav2/monitoring/state.py
+++ syriav2/monitoring/state.py
@@ -1,1 +1,28 @@
+from abc import ABC, abstractmethod
+from .models import ProjectStageUpdate
+from projects.models import Project
 
+class ProjectStageState(ABC):
+    @abstractmethod
+    def handle(self, update: ProjectStageUpdate):
+        pass
+
+class StageOneState(ProjectStageState):
+    def handle(self, update):
+        update.project.status = 'in_progress'
+        update.project.save()
+
+class StageSevenState(ProjectStageState):
+    def handle(self, update):
+        update.project.status = 'completed'
+        update.project.is_cancelled = False
+        update.project.save()
+
+
+class StageStateFactory:
+    @staticmethod
+    def get_state(stage_number):
+        if stage_number == 7:
+            return StageSevenState()
+        else:
+            return StageOneState()  
