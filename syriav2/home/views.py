ebe4553f07c8898ed8567d76bef4a9f4a72b40a2
banaalsawaf1 <banaalsawaf1@gmail.com>
2026-01-11 18:36:49 +0300

    Update views.py

--- syriav2/home/views.py
+++ syriav2/home/views.py
@@ -1,5 +1,33 @@
 from django.shortcuts import render
 from projects.models import Project 
-# Create your views here.
+
 def index(request):
-    return render(request, 'home/index.html')
+   
+    priority_projects = Project.objects.filter(
+        priority_score__gt=0
+    ).order_by('-priority_score')[:3]
+
+    
+    
+    ordered_projects = []
+    labels = ["أهم مشروع", "متوسط الأهمية", "أقل أهمية"]
+    icons = ["fas fa-crown text-warning", "fas fa-medal text-secondary", "fas fa-award text-info"]
+    
+    for i, proj in enumerate(priority_projects):
+        ordered_projects.append({
+            'project': proj,
+            'level': labels[i],
+            'icon': icons[i],
+            'rank': i + 1
+        })
+
+    
+    in_progress_projects = Project.objects.filter(
+         status__in=['in_progress', 'completed'],
+        is_cancelled=False
+    )
+    context = {
+        'priority_projects': ordered_projects,
+        'in_progress_projects': in_progress_projects, 
+    }
+    return render(request, 'home/index.html', context)
