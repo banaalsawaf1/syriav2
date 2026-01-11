b4c28bc976e909d9e862d3972e671dabf54a4b6e
banaalsawaf1 <banaalsawaf1@gmail.com>
2026-01-11 18:40:18 +0300

    Update urls.py

--- syriav2/monitoring/urls.py
+++ syriav2/monitoring/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'monitoring'
+
 urlpatterns = [
-    # نضيف المسارات لاحقًا
+    path('update/', views.project_stage_update_view, name='project_stages_update'),
+    path('detail/<int:project_id>/', views.project_stages_detail_view, name='project_stages_detail'),
+    path('admin/updates/', views.admin_updates_management_view, name='admin_updates_management'),
 ]
